81ac4d2a8608650d10ebc291e0a0bfc6764a6f97
mfouda214 <m.fouda@robone.net>
2018-07-31 22:20:33 +0100

    Add pushing property

--- CustomTransitionDemo/CustomNavigationAnimator.swift
+++ CustomTransitionDemo/CustomNavigationAnimator.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class CustomNavigationAnimator: NSObject, UIViewControllerAnimatedTransitioning {
     
+    var pushing = false
+    
     func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
         return 1
     }
